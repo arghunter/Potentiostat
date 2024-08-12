f45bbb1b3012ed82cc55ded1062f8c303286b6cd
Armaan Gomes <91099806+arghunter@users.noreply.github.com>
2024-08-11 21:04:07 -0700

    Update potentiostat.ino

--- src/potentiostat.ino
+++ src/potentiostat.ino
@@ -5,12 +5,12 @@ const int analogPins[] = {A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12,
 const int numPins = 18;
 const int sampleRate =10000; // 10kHz
 const int bufferSize = numPins * 2; // Each reading is 2 bytes
-const int F=2;//Frequency
-const int Fs = 500;//Sample Rate
-const int N=500;//Num Samples
-const int pwmPinOut = 8;
+const int F=1000;//Frequency
+const int Fs = 3000;//Sample Rate
+const int N=(int)(Fs/F);//Num Samples
+const int pwmPinOut = 12;
 
-const int sampleInterval=1000000/(F*N);
+// const int sampleInterval=1000000/(F*N);
 
 
 uint8_t buffer1[2000][bufferSize];
@@ -18,7 +18,7 @@ volatile bool bufferReady = false;
 volatile int bufferHead=0;
 volatile int bufferTail=2;
 volatile int bufferIndex = 0;
-volatile uint8_t samples[N];
+volatile uint16_t samples[N];
 
 // Timer interrupt to read analog values
 IntervalTimer sampleTimer;
@@ -27,8 +27,8 @@ uint16_t counter=0;
 void sampleData() {
   for (int i = 0; i < numPins; i++) {
     int sensorValue = analogRead(analogPins[i]);
-    buffer1[bufferTail][bufferIndex++] = sensorValue & 0xFF; // Lower byte
-    buffer1[bufferTail][bufferIndex++] = sensorValue>>8; // Upper byte
+    buffer1[bufferTail][bufferIndex++] = counter & 0xFF; // Lower byte
+    buffer1[bufferTail][bufferIndex++] = counter>>8; // Upper byte
   }
 
   counter+=1;
@@ -39,19 +39,24 @@ void sampleData() {
   }
 
 }
-void sendSignal(){
-  for(int i=0;i<N;i++){
-    analogWrite(pwmPinOut, samples[i]);
+void sendSignal() {
+  static int index = 0;
+
+  analogWrite(pwmPinOut, (int)samples[index]); // Output the current sample
+
+  index++;
+  if (index >= N) {
+    index = 0; // Loop back to the beginning of the sine wave
   }
 }
 void setup() {
   Serial.begin(115200);
-  while (!Serial); // Wait for the Serial port to be available
-  pinMode(8, OUTPUT);
-  int t;
+
+  
+
   for(int i=0;i<N;i++){
-    t = (float) i/Fs;
-    samples[i]= (uint8_t) (127.0 * sin(2*3.1415*F*t) + 127.0);
+    
+    samples[i] = 2048 + 2047 * sin(2 * PI * i / N);
   }
   // Initialize the analog pins
   for (int i = 0; i < numPins; i++) {
@@ -60,25 +65,26 @@ void setup() {
   // Configure the ADC for high speed
   analogReadResolution(12); // 12-bit resolution
   analogReadAveraging(1);   // No averaging
-
+  analogWriteResolution(12);
   // Set up the timer interrupt to sample data at the samplerate
+  outTimer.begin(sendSignal,1000000 / Fs);
   sampleTimer.begin(sampleData, 1000000 / sampleRate);
-  outTimer.begin(sendSignal, sampleInterval);
-  delay(2);
+
+  // delay(2);
 }
 
 void loop() {
   
-  
-    while(bufferHead!=bufferTail){
+  delay(1);
+  while(bufferHead!=bufferTail){
 
-      Serial.write( buffer1[bufferHead],36);
-      bufferHead++;
-      if(bufferHead>=2000){
-        bufferHead=0;
-      }
+    Serial.write( buffer1[bufferHead],36);
+    bufferHead++;
+    if(bufferHead>=2000){
+      bufferHead=0;
     }
+  }
 
 }
-  // Wait a bit before checking the buffer again
+ 
   
